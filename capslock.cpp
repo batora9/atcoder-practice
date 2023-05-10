8e9ee20e49e1a5afcbf7814640814cfc32fc6cfe
syun_m <sm.net_minecraft.hacker@outlook.jp>
2023-05-11 02:24:48 +0900

    入門講習会のコード

---
+++ capslock.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    char add;
+    string out = "";
+    cin >> str;
+    int l = str.size();
+    for (int i = 0; l > i; i++)
+    {
+        add = toupper(str[i]);
+        out += add;
+    }
+    cout << out;
+}
